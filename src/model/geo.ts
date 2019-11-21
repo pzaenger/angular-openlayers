ac9b935bcfbfee55321f103887ec63ca9b14159d
pzaenger <dev@pzaenger.com>
2019-11-21 16:13:34 +0100

    naming and title fixed; preview added

--- src/model/geo.ts
+++ src/model/geo.ts
@@ -1,9 +1,11 @@
-import OlMap from 'ol/Map';
-import OlView from 'ol/View';
-import OlTileLayer from 'ol/layer/Tile';
-import OlXYZ from 'ol/source/XYZ';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import XYZ from 'ol/source/XYZ';
+import ZoomToExtent from 'ol/control/ZoomToExtent';
 
 import { fromLonLat } from 'ol/proj';
+import { defaults as defaultControls } from 'ol/control';
 
 /**
  * Geo.
@@ -11,26 +13,34 @@ import { fromLonLat } from 'ol/proj';
 export class Geo {
 
   /* Map. */
-  public readonly olMap: OlMap;
+  public readonly map: Map;
 
   /**
    * Initialise the map.
    * @param view View with initial center and zoom.
    */
   constructor(view: { zoom: number; center: [number, number]; } = { zoom: 2, center: [0, 0] }) {
-    this.olMap = new OlMap({
+    this.map = new Map({
       target: 'map',
       layers: [
-        new OlTileLayer({
-          source: new OlXYZ({
+        new TileLayer({
+          source: new XYZ({
             url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
           })
         })
       ],
-      view: new OlView({
+      view: new View({
         center: fromLonLat(view.center),
         zoom: view.zoom
-      })
+      }),
+      controls: defaultControls().extend([
+        new ZoomToExtent({
+          extent: [
+            813079.7791264898, 5929220.284081122,
+            848966.9639063801, 5936863.986909639
+          ]
+        })
+      ])
     });
   }
 }
