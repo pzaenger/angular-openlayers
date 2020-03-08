4e6d45545558b87a842a4667ae4002da22a13c3f
pzaenger <dev@pzaenger.com>
2020-03-08 17:05:04 +0100

    Added some controls and improved source handling for the base layer

--- src/model/geo.ts
+++ src/model/geo.ts
@@ -1,37 +1,51 @@
 import Map from 'ol/Map';
 import View from 'ol/View';
-import TileLayer from 'ol/layer/Tile';
-import XYZ from 'ol/source/XYZ';
+import LayerTile from 'ol/layer/Tile';
 import ZoomToExtent from 'ol/control/ZoomToExtent';
-
+import FullScreen from 'ol/control/FullScreen';
+import ScaleLine from 'ol/control/ScaleLine';
+import Attribution from 'ol/control/Attribution';
+import SourceOsm from 'ol/source/OSM';
+import SourceStamen from 'ol/source/Stamen';
 import { fromLonLat } from 'ol/proj';
 import { defaults as defaultControls } from 'ol/control';
 import { defaults as defaultInteractions, PinchZoom } from 'ol/interaction';
 
 /**
- * Geo.
+ * Geographical map containing some basic controls and two sources for its base layer.
  */
 export class Geo {
 
-  /* Map. */
-  public readonly map: Map;
+  /** Map. */
+  readonly map: Map;
+
+  /** Layer. */
+  readonly layerTile: LayerTile;
+
+  /** Sources. */
+  readonly sources: { readonly osm: SourceOsm; readonly stamen: SourceStamen; };
 
   /**
    * Initialise the map.
    * @param view View with initial center and zoom.
    */
   constructor(view: { zoom: number; center: [number, number]; } = { zoom: 2, center: [0, 0] }) {
+    this.sources = {
+      osm: new SourceOsm(),
+      stamen: new SourceStamen({ layer: 'toner' })
+    };
+
+    this.layerTile = new LayerTile({
+      source: this.sources.osm
+    });
+
     this.map = new Map({
       target: 'map',
       interactions: defaultInteractions().extend([
         new PinchZoom()
       ]),
       layers: [
-        new TileLayer({
-          source: new XYZ({
-            url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-          })
-        })
+        this.layerTile
       ],
       view: new View({
         center: fromLonLat(view.center),
@@ -39,11 +53,17 @@ export class Geo {
         constrainResolution: true
       }),
       controls: defaultControls().extend([
+        new Attribution(),
         new ZoomToExtent({
           extent: [
             813079.7791264898, 5929220.284081122,
             848966.9639063801, 5936863.986909639
           ]
+        }),
+        new FullScreen(),
+        new ScaleLine({
+          bar: true,
+          minWidth: 150
         })
       ])
     });
@@ -57,5 +77,13 @@ export class Geo {
   setView(zoom: number, center: [number, number]) {
     this.map.getView().setZoom(10);
     this.map.getView().setCenter(fromLonLat(center));
+  }
+
+  /**
+   * Sets the source of the base layer.
+   * @param source Source.
+   */
+  setSource(source: 'osm' | 'stamen') {
+    this.layerTile.setSource(this.sources[source]);
   }
 }
